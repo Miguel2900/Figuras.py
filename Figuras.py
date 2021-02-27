a6b16cb301402ccc9a638965c1ae358a411e06f0
Miguel2900 <miguel.ledesmao9@gmail.com>
2021-02-26 22:46:21 -0600

    Release

--- Figuras.py
+++ Figuras.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-# screen parameters   hola dany
+# screen parameters   hola andre fgfhg
 SIZE = (900, 500)
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('MIS PRIMERAS FIGURAS')
